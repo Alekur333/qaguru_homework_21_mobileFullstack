daa3ed4fa5bc865b968b1fc39f26daefe9b33528
Alekur333 <amastar963@gmail.com>
2022-02-16 23:36:33 +0500

    added SelenoidConfig

---
+++ src/test/java/config/SelenoidConfig.java
@@ -0,0 +1,28 @@
+package config;
+
+import org.aeonbits.owner.Config;
+
+@Config.LoadPolicy(Config.LoadType.MERGE)
+@Config.Sources({
+        "system:properties",
+        "classpath:config/selenoid.properties"
+})
+public interface SelenoidConfig extends Config {
+    String userName();
+
+    String password();
+
+    String url();
+
+    String deviceName();
+
+    String version();
+
+    String app();
+
+    String platformName();
+
+    String appPackage();
+
+    String appActivity();
+}
--- src/test/java/kur/alexei/drivers/SelenoidMobileDriver.java
+++ src/test/java/kur/alexei/drivers/SelenoidMobileDriver.java
@@ -3,6 +3,7 @@ package kur.alexei.drivers;
 
 import com.codeborne.selenide.WebDriverProvider;
 import config.RealConfig;
+import config.SelenoidConfig;
 import io.appium.java_client.android.AndroidDriver;
 import org.aeonbits.owner.ConfigFactory;
 import org.openqa.selenium.WebDriver;
@@ -15,9 +16,9 @@ import static kur.alexei.utils.FileUtils.getAbsolutePath;
 
 
 public class SelenoidMobileDriver implements WebDriverProvider {
-    public static RealConfig config = ConfigFactory.create(RealConfig.class, System.getProperties());
+    public static SelenoidConfig config = ConfigFactory.create(SelenoidConfig.class, System.getProperties());
 
-    public static URL getBrowserstackUrl() {
+    public static URL getSelenoidUrl() {
         try {
             return new URL(config.url());
         } catch (MalformedURLException e) {
@@ -30,6 +31,7 @@ public class SelenoidMobileDriver implements WebDriverProvider {
 
         desiredCapabilities.setCapability("app",
                 getAbsolutePath(config.app()));
+//        desiredCapabilities.setCapability("app", apkUrl());
         desiredCapabilities.setCapability("locale", "en");
         desiredCapabilities.setCapability("language", "en");
         desiredCapabilities.setCapability("platformName", config.platformName());
@@ -37,6 +39,17 @@ public class SelenoidMobileDriver implements WebDriverProvider {
         desiredCapabilities.setCapability("appPackage", config.appPackage());
         desiredCapabilities.setCapability("appActivity", config.appActivity());
 
-        return new AndroidDriver(getBrowserstackUrl(), desiredCapabilities);
+        return new AndroidDriver(getSelenoidUrl(), desiredCapabilities);
     }
+
+
+    private URL apkUrl() {
+        try {
+            return new URL("https://github.com/wikimedia/apps-android-wikipedia/releases/download/latest/app-alpha-universal-release.apk");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
